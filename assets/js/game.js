885f5f683de5190c2f2cd292cdf0d58acd7621eb
jesusnboone <jesusnboone@gmail.com>
2020-06-02 22:51:54 -0500

    fixed for loop & enemy health

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,8 +8,11 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+
+    while(enemyHealth > 0) {
+
     // Alert users that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -67,8 +70,10 @@ var fight = function(enemyName) {
         window.alert("You need to pick a valid option. Try again!");
     }
     
-}
+} }
 
 for(var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
     fight(enemyNames[i]);
 }
